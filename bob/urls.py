3b1d5de381c01e17e554ee84dd8a17884a5e56ef
liujinliu <liujinliu@lbesec.com>
2018-02-26 17:12:11 +0800

    fix code bug, and use class based view

--- bob/urls.py
+++ bob/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-
+from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 app_name = 'bob'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:task_name>/gogo/', views.gogo, name='gogo'),
-    path('<str:task_name>/bobtasks/', views.bobtasks, name='bobtasks'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<str:task_name>/gogo/', views.GogoView.as_view(), name='gogo'),
+    path('<str:task_name>/bobtasks/', views.BobTaskView.as_view(), name='bobtasks'),
     path('<str:task_name>/bobtasks/plain/<int:status>/',
-         views.bobtasks_plaintext, name='bobtasks_plaintext'),
+         views.BobTaskPlainView.as_view(), name='bobtasks_plaintext'),
     path('<str:task_name>/bobtasks/<int:id>/',
-         views.bobtasks_update, name='bobtasks_update'),
+         csrf_exempt(views.BobTaskUpdateView.as_view()), name='bobtasks_update'),
     path('fileload/<str:task_name>/<str:in_out>/<str:filename>/',
          views.file_download, name='file_download'),
 ]
--- bob/views.py
+++ bob/views.py
@@ -8,86 +8,9 @@ from django.urls import reverse
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.http import StreamingHttpResponse
+from django.views import View
 from .models import Tasks, BobTasks
 from .utils import handle_upload_file
-
-
-def index(request):
-    all_task = Tasks.objects.order_by('cdate')
-    if len(all_task) > 0:
-        t = all_task[0]
-        all_t = []
-        for t in all_task:
-            all_t.append(dict(current_name=t.name,
-                              current_fields=t.fields.split(",")))
-        context = {
-            'all_names': list(map(lambda x: x.name, all_task)),
-            'all_t': all_t
-        }
-        return render(request, 'bob/index.html', context)
-    else:
-        return HttpResponse("No Task found, first add a task via admin page")
-
-
-def gogo(request, task_name):
-    task = get_object_or_404(Tasks, name=task_name)
-    fields = task.fields.split(",") # NOQA
-    paras = {}
-    for field in fields:
-        paras[field] = request.POST[field]
-    input_file = ""
-    filekey = 'input-file'
-    if request.FILES.get(filekey) and str(request.FILES[filekey]):
-        input_file = handle_upload_file(request.FILES[filekey], task_name,
-                                        str(request.FILES[filekey]))
-    bob_task = BobTasks(name=task_name, cdate=timezone.now(),
-                        udate=timezone.now(), para=json.dumps(paras),
-                        input_file=input_file, output_file="", output="")
-    bob_task.save()
-    return HttpResponseRedirect(reverse('bob:bobtasks', args=(task_name,)))
-
-
-def bobtasks(request, task_name):
-    bobtask = get_list_or_404(BobTasks, name=task_name)
-    context = {'name': task_name, 'tasks': bobtask}
-    return render(request, 'bob/bobtask.html', context)
-
-
-def bobtasks_plaintext(request, task_name, status):
-    bobtask = get_list_or_404(BobTasks, name=task_name, status=status)
-    if not bobtask:
-        return HttpResponse("no task found")
-    ret = []
-    for t in bobtask:
-        para = json.loads(t.para)
-        input_file = t.input_file
-        ret.append(dict(id=t.id, para=para, input_file=input_file))
-    return HttpResponse(json.dumps(ret))
-
-
-@csrf_exempt
-def bobtasks_update(request, task_name, id):
-    if not request.POST.get("status"):
-        return HttpResponse("Error, need status")
-    bobtask = get_object_or_404(BobTasks, name=task_name, id=id)
-    if bobtask:
-        status = request.POST.get('status', 0)
-        bobtask.status = int(status)
-        bobtask.output = request.POST.get('output', '')
-        output_file_fullpath = request.POST.get('output_file', '')
-        filename = os.path.basename(output_file_fullpath)
-        target_path = "./%s/output/" % task_name
-        if not os.path.exists(target_path):
-            os.makedirs(target_path)
-        shutil.copy(output_file_fullpath, target_path + filename)
-        bobtask.output_file = filename
-        bobtask.save()
-        return HttpResponse(("set task:{task_name},id:{id} status to {status}"
-                             "output:{output}, output_file:{output_file}").format(
-                            task_name=task_name, id=id, status=status,
-                            output=bobtask.output, output_file=bobtask.output_file))
-    else:
-        return HttpResponse("unkonw id")
 
 
 def file_download(request, task_name, in_out, filename):
@@ -104,3 +27,90 @@ def file_download(request, task_name, in_out, filename):
     response['Content-Type'] = 'application/octet-stream'
     response['Content-Disposition'] = 'attachment;filename="{0}"'.format(filename)
     return response
+
+
+class IndexView(View):
+
+    def get(self, request):
+        all_task = Tasks.objects.order_by('cdate')
+        if len(all_task) > 0:
+            t = all_task[0]
+            all_t = []
+            for t in all_task:
+                all_t.append(dict(current_name=t.name,
+                                  current_fields=t.fields.split(",")))
+            context = {
+                'all_names': list(map(lambda x: x.name, all_task)),
+                'all_t': all_t
+            }
+            return render(request, 'bob/index.html', context)
+        else:
+            return HttpResponse("No Task found, first add a task via admin page")
+
+
+class GogoView(View):
+
+    def post(self, request, task_name):
+        task = get_object_or_404(Tasks, name=task_name)
+        fields = task.fields.split(",") # NOQA
+        paras = {}
+        for field in fields:
+            paras[field] = request.POST[field]
+        input_file = ""
+        filekey = 'input-file'
+        if request.FILES.get(filekey) and str(request.FILES[filekey]):
+            input_file = handle_upload_file(request.FILES[filekey], task_name,
+                                            str(request.FILES[filekey]))
+        bob_task = BobTasks(name=task_name, cdate=timezone.now(),
+                            udate=timezone.now(), para=json.dumps(paras),
+                            input_file=input_file, output_file="", output="")
+        bob_task.save()
+        return HttpResponseRedirect(reverse('bob:bobtasks', args=(task_name,)))
+
+
+class BobTaskView(View):
+
+    def get(self, request, task_name):
+        bobtask = get_list_or_404(BobTasks, name=task_name)
+        context = {'name': task_name, 'tasks': bobtask}
+        return render(request, 'bob/bobtask.html', context)
+
+
+class BobTaskPlainView(View):
+
+    def get(self, request, task_name, status):
+        bobtask = get_list_or_404(BobTasks, name=task_name, status=status)
+        if not bobtask:
+            return HttpResponse("no task found")
+        ret = []
+        for t in bobtask:
+            para = json.loads(t.para)
+            input_file = t.input_file
+            ret.append(dict(id=t.id, para=para, input_file=input_file))
+        return HttpResponse(json.dumps(ret))
+
+
+class BobTaskUpdateView(View):
+
+    def post(self, request, task_name, id):
+        if not request.POST.get("status"):
+            return HttpResponse("Error, need status")
+        bobtask = get_object_or_404(BobTasks, name=task_name, id=id)
+        if bobtask:
+            status = request.POST.get('status', 0)
+            bobtask.status = int(status)
+            bobtask.output = request.POST.get('output', '')
+            output_file_fullpath = request.POST.get('output_file', '')
+            filename = os.path.basename(output_file_fullpath)
+            target_path = "./%s/output/" % task_name
+            if not os.path.exists(target_path):
+                os.makedirs(target_path)
+            shutil.copy(output_file_fullpath, target_path + filename)
+            bobtask.output_file = filename
+            bobtask.save()
+            return HttpResponse(("set task:{task_name},id:{id} status to {status}"
+                                 "output:{output}, output_file:{output_file}").format(
+                                task_name=task_name, id=id, status=status,
+                                output=bobtask.output, output_file=bobtask.output_file))
+        else:
+            return HttpResponse("unkonw id")
