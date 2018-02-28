f16fa7882fc7d0c81ef1ab33eaaa9e5f48cabb19
liujinliu <liujinliu@lbesec.com>
2018-02-28 13:58:14 +0800

    modify version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bobgogo',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     include_package_data=True,
     description='A simple Django app to record tasks for you.',
